dbb1dee0a0ed5e282a2991cad49336916e87d1e6
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-10-15 11:17:43 +0700

    add profile

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -3,8 +3,10 @@ import 'package:get/get.dart';
 import 'package:movieapp/bindings/Trending_Bindings.dart';
 import 'package:movieapp/bindings/Tv_Bindings.dart';
 import 'package:movieapp/bindings/movie_bindings.dart';
+import 'package:movieapp/bindings/profile_bindings.dart';
 import 'package:movieapp/bindings/search_bindings.dart';
 import 'package:movieapp/screens/movie/Movie_Screens.dart';
+import 'package:movieapp/screens/profile/Profile_Screens.dart';
 import 'package:movieapp/screens/search/Search_Screens.dart';
 import 'package:movieapp/screens/trending/Trending_Screen.dart';
 import 'package:movieapp/screens/tv/Tv_Screen.dart';
@@ -13,7 +15,7 @@ class HomeController extends GetxController {
   static HomeController get to => Get.find();
   var tabIndex = 0.obs;
 
-  final pages = <String>['/movie', '/trending', '/tv', '/search'];
+  final pages = <String>['/movie', '/trending', '/tv', '/search', '/profile'];
 
   void changeTabIndex(int idx) {
     tabIndex.value = idx;
@@ -50,6 +52,13 @@ class HomeController extends GetxController {
           settings: settings,
           page: () => SearchScreens(),
           binding: SearchBindings());
+    }
+
+    if (settings.name == '/profile') {
+      return GetPageRoute(
+          settings: settings,
+          page: () => ProfileScreen(),
+          binding: ProfileBindings());
     }
 
     return null;
