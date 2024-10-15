d5b8c01c42296e1254e4d2bfb75007bb7647e736
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-10-15 14:41:31 +0700

    add button

--- lib/screens/profile/Profile_Screens.dart
+++ lib/screens/profile/Profile_Screens.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:movieapp/controller/profile_controller.dart';
 import 'package:movieapp/utils/theme.dart';
+import 'package:movieapp/widgets/Button_Profile.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -64,7 +65,18 @@ class _ProfileScreenState extends State<ProfileScreen> {
                           fontSize: 18,
                           fontWeight: FontWeight.w700,
                         ),
-                      )
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ButtonProfile(
+                          title: 'Favorite',
+                          icon: Icons.favorite,
+                          onTap: () {}),
+                      ButtonProfile(
+                          title: 'Rated', icon: Icons.stars, onTap: () {}),
+                      ButtonProfile(
+                          title: 'Watchlist', icon: Icons.tv, onTap: () {})
                     ],
                   )),
             ));
