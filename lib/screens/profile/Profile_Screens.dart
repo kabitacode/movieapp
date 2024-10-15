b0d0da51e6b592075e1b7c3a3b365e0e49566a01
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-10-15 11:18:42 +0700

    add profile screens

--- lib/screens/profile/Profile_Screens.dart
+++ lib/screens/profile/Profile_Screens.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:movieapp/controller/profile_controller.dart';
+import 'package:movieapp/utils/theme.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -8,8 +12,63 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  late ProfileController _profileController;
+
+  @override
+  void initState() {
+    super.initState();
+    _profileController = Get.put(ProfileController());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        backgroundColor: AppColors.background,
+        body: Obx(() {
+          if (_profileController.isLoading.value) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            String path =
+                _profileController.data['avatar']?['tmdb']?['avatar_path'];
+            String image =
+                path != null ? 'https://image.tmdb.org/t/p/w500$path' : '';
+
+            return SafeArea(
+                child: SingleChildScrollView(
+              child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Center(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(50),
+                          child: Image.network(
+                            image,
+                            width: 100.0,
+                            height: 100.0,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        textAlign: TextAlign.center,
+                        _profileController.data['name'] ??
+                            _profileController.data['username'],
+                        style: GoogleFonts.openSans(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      )
+                    ],
+                  )),
+            ));
+          }
+        }));
   }
 }
