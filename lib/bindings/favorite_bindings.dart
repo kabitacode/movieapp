abbc528c96f6f080a1c47f77949c08755dde9493
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-10-16 14:26:22 +0700

    favorite tv movie

--- lib/bindings/favorite_bindings.dart
+++ lib/bindings/favorite_bindings.dart
@@ -2,8 +2,11 @@ import 'package:get/get.dart';
 import 'package:movieapp/controller/favorite_screen_controller.dart';
 
 class FavoriteBindings extends Bindings {
+  final String selectFavorite;
+  FavoriteBindings({required this.selectFavorite});
+
   @override
   void dependencies() {
-    Get.lazyPut(() => FavoriteScreenController());
+    Get.lazyPut(() => FavoriteScreenController(selectFavorite: selectFavorite));
   }
 }
