dd3936910debdfedf23f545a5986f03d4bdf9a5c
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-10-16 14:01:24 +0700

    add watchlist controller screen

--- lib/controller/watchlist_screen_controller.dart
+++ lib/controller/watchlist_screen_controller.dart
@@ -1,3 +1,52 @@
-import 'package:get/get.dart';
+import 'dart:convert';
 
-class WatchlistController extends GetxController {}
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
+class WatchlistScreenController extends GetxController {
+  var isLoading = false.obs;
+  var list = [].obs;
+  String? api_key = dotenv.env['API_KEY'];
+  var account_id = dotenv.env['ACCOUNT_ID'];
+  String? access_token = dotenv.env['ACCESS_TOKEN'];
+  var page = 1;
+
+  @override
+  void onInit() {
+    super.onInit();
+    getApi();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void getApi({int? page}) async {
+    var url =
+        'https://api.themoviedb.org/3/account/$account_id/watchlist/movies?language=en-US&page=${page ?? this.page}';
+
+    try {
+      isLoading.value = true;
+      final res = await http.get(Uri.parse(url), headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $access_token'
+      });
+      final result = await json.decode(res.body);
+      if (res.statusCode == 200) {
+        if (page == 1) {
+          list.value = result['results'];
+        } else {
+          list.addAll(result['results']);
+        }
+      } else {
+        Get.snackbar('error', 'Oops something error!');
+      }
+    } catch (e) {
+      Get.snackbar('error', 'Failed: $e');
+    } finally {
+      isLoading.value = false;
+    }
+  }
+}
