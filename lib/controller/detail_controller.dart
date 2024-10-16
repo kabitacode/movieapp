eca5816237b56ff66b681ee06192523e72ab4d77
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-10-16 11:01:00 +0700

    fix favorites & add watchlist controller

--- lib/controller/detail_controller.dart
+++ lib/controller/detail_controller.dart
@@ -36,7 +36,7 @@ class DetailController extends GetxController {
         //     'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYWMzODcwMzRhNDA5OTA1YjgxMTg1MDdkMzhkYTU5MyIsIm5iZiI6MTcyODQ0MTA3OS40ODEwMjUsInN1YiI6IjY3MDRlNGFkNGIwYzViOWQ3MTY5Yzk4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nFpg8WE3KBIwywq197_baJqKevk46HhrVNiFqq2_gMs'
       });
       var result = json.decode(res.body);
-      print(res.request);
+
       if (res.statusCode == 200) {
         list.value = result;
       } else {
