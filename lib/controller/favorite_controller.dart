5fad0509abdbb3606d0759ec4c90d91293f15314
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-10-16 13:41:41 +0700

    bool default false loading

--- lib/controller/favorite_controller.dart
+++ lib/controller/favorite_controller.dart
@@ -7,7 +7,7 @@ import 'package:http/http.dart' as http;
 class FavoriteController extends GetxController {
   String? access_token = dotenv.env['ACCESS_TOKEN'];
   var account_id = dotenv.env['ACCOUNT_ID'];
-  var isLoading = true.obs;
+  var isLoading = false.obs;
   var data = {}.obs;
   var watchList = {}.obs;
 
