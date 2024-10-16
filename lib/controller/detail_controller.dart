d292720ac7ce1e76b6b32f200d45be8f20b6464d
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-10-16 14:31:02 +0700

    🔥

--- lib/controller/detail_controller.dart
+++ lib/controller/detail_controller.dart
@@ -32,11 +32,9 @@ class DetailController extends GetxController {
     try {
       final res = await http.get(Uri.parse(baseUrl), headers: {
         'Content-Type': 'application/json',
-        // "Authorization":
-        //     'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYWMzODcwMzRhNDA5OTA1YjgxMTg1MDdkMzhkYTU5MyIsIm5iZiI6MTcyODQ0MTA3OS40ODEwMjUsInN1YiI6IjY3MDRlNGFkNGIwYzViOWQ3MTY5Yzk4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nFpg8WE3KBIwywq197_baJqKevk46HhrVNiFqq2_gMs'
       });
       var result = json.decode(res.body);
-
+      print(res.request);
       if (res.statusCode == 200) {
         list.value = result;
       } else {
