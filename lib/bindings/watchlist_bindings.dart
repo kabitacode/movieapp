27cf587c6e68e85d6e4e9ccba94393f538c9e88c
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-10-18 14:12:19 +0700

    ✨ add watchlist tv movie

--- lib/bindings/watchlist_bindings.dart
+++ lib/bindings/watchlist_bindings.dart
@@ -2,8 +2,12 @@ import 'package:get/get.dart';
 import 'package:movieapp/controller/watchlist_screen_controller.dart';
 
 class WatchlistBindings extends Bindings {
+  final String selectWatchlist;
+  WatchlistBindings({required this.selectWatchlist});
+
   @override
   void dependencies() {
-    Get.lazyPut(() => WatchlistScreenController());
+    Get.lazyPut(
+        () => WatchlistScreenController(selectwatchlist: selectWatchlist));
   }
 }
