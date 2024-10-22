3df587f4abd1b3b50950ee30e9eed5cd53d60412
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-10-22 10:49:30 +0700

    screen add position fixed sticky

--- lib/screens/popular/Popular_Screen.dart
+++ lib/screens/popular/Popular_Screen.dart
@@ -4,6 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:movieapp/controller/popular_controller.dart';
 import 'package:movieapp/screens/detail/Detail_Screen.dart';
 import 'package:movieapp/utils/theme.dart';
+import 'package:movieapp/widgets/Button_Back.dart';
 
 class PopularScreen extends StatefulWidget {
   const PopularScreen({super.key});
@@ -44,146 +45,136 @@ class _PopularScreenState extends State<PopularScreen> {
             );
           } else {
             return SafeArea(
-              child: SingleChildScrollView(
-                controller: _scrollController,
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 10,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Get.back();
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          children: [
-                            Icon(
-                              size: 15,
-                              Icons.arrow_back_ios,
-                              color: Colors.white,
-                            ),
-                            Text(
-                              "Back",
-                              style: GoogleFonts.openSans(
-                                color: Colors.white,
-                                fontSize: 15,
-                                fontWeight: FontWeight.w700,
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                    Text(
-                      "Popular",
-                      style: GoogleFonts.openSans(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text(
-                      "List of good popular movies",
-                      style: GoogleFonts.poppins(
-                        color: Colors.white,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w300,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: GridView.builder(
-                          physics: NeverScrollableScrollPhysics(),
-                          shrinkWrap: true,
-                          gridDelegate:
-                              SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: 2,
-                                  crossAxisSpacing: 15,
-                                  mainAxisSpacing: 20,
-                                  childAspectRatio: 0.7),
-                          itemCount: controller.list.length,
-                          itemBuilder: (context, index) {
-                            final item = controller.list[index];
+                child: Stack(
+              children: [
+                SingleChildScrollView(
+                  controller: _scrollController,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 10, right: 10, top: 120),
+                    child: GridView.builder(
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            crossAxisSpacing: 15,
+                            mainAxisSpacing: 20,
+                            childAspectRatio: 0.7),
+                        itemCount: controller.list.length,
+                        itemBuilder: (context, index) {
+                          final item = controller.list[index];
 
-                            return InkWell(
-                                onTap: () {
-                                  Get.to(() =>
-                                      DetailScreen(movieId: item.id ?? 0));
-                                },
-                                child: Container(
-                                  padding: EdgeInsets.all(8),
-                                  decoration: BoxDecoration(
-                                      color: Colors.white,
-                                      borderRadius: BorderRadius.circular(8.0),
-                                      boxShadow: [
-                                        BoxShadow(
-                                            color: Colors.grey.withOpacity(0.3),
-                                            spreadRadius: 2,
-                                            blurRadius: 5,
-                                            offset: Offset(0, 3))
-                                      ]),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Expanded(
-                                          child: item.posterPath != null
-                                              ? Image.network(
-                                                  width: double.infinity,
-                                                  'https://image.tmdb.org/t/p/w500${item.posterPath}',
-                                                  fit: BoxFit.cover,
-                                                )
-                                              : Icon(
-                                                  Icons.image_not_supported,
-                                                  size: 50,
-                                                )),
-                                      SizedBox(
-                                        height: 8,
+                          return InkWell(
+                              onTap: () {
+                                Get.to(
+                                    () => DetailScreen(movieId: item.id ?? 0));
+                              },
+                              child: Container(
+                                padding: EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(8.0),
+                                    boxShadow: [
+                                      BoxShadow(
+                                          color: Colors.grey.withOpacity(0.3),
+                                          spreadRadius: 2,
+                                          blurRadius: 5,
+                                          offset: Offset(0, 3))
+                                    ]),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Expanded(
+                                        child: item.posterPath != null
+                                            ? Image.network(
+                                                width: double.infinity,
+                                                'https://image.tmdb.org/t/p/w500${item.posterPath}',
+                                                fit: BoxFit.cover,
+                                              )
+                                            : Icon(
+                                                Icons.image_not_supported,
+                                                size: 50,
+                                              )),
+                                    SizedBox(
+                                      height: 8,
+                                    ),
+                                    Center(
+                                      child: Text(
+                                        item.title ?? 'No Title Available',
+                                        style: TextStyle(
+                                            fontSize: 13,
+                                            color: AppColors.typography,
+                                            fontWeight: FontWeight.w600),
+                                        overflow: TextOverflow.ellipsis,
+                                        maxLines: 1,
                                       ),
-                                      Center(
-                                        child: Text(
-                                          item.title ?? 'No Title Available',
-                                          style: TextStyle(
-                                              fontSize: 13,
-                                              color: AppColors.typography,
-                                              fontWeight: FontWeight.w600),
-                                          overflow: TextOverflow.ellipsis,
-                                          maxLines: 1,
-                                        ),
+                                    ),
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    Center(
+                                      child: Text(
+                                        item.subtitle ??
+                                            'No Subtitle Available',
+                                        style: TextStyle(fontSize: 10),
+                                        overflow: TextOverflow.ellipsis,
+                                        maxLines: 1,
                                       ),
-                                      SizedBox(
-                                        height: 5,
-                                      ),
-                                      Center(
-                                        child: Text(
-                                          item.subtitle ??
-                                              'No Subtitle Available',
-                                          style: TextStyle(fontSize: 10),
-                                          overflow: TextOverflow.ellipsis,
-                                          maxLines: 1,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ));
-                          }),
-                    ),
-                    if (controller.isLoading.value)
-                      Padding(
-                          padding: EdgeInsets.all(10),
-                          child: CircularProgressIndicator())
-                  ],
+                                    ),
+                                  ],
+                                ),
+                              ));
+                        }),
+                  ),
                 ),
-              ),
-            );
+                if (controller.isLoading.value)
+                  Positioned(
+                      top: 150,
+                      left: 50,
+                      right: 50,
+                      child: Container(
+                        width: 250,
+                        height: 250,
+                        color: AppColors.background,
+                        child: const Padding(
+                            padding: EdgeInsets.all(10),
+                            child: CircularProgressIndicator()),
+                      )),
+                Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      color: AppColors.background,
+                      padding: EdgeInsets.only(bottom: 20),
+                      child: Column(
+                        children: [
+                          Container(
+                            child: ButtonBack(onTap: () => Get.back()),
+                          ),
+                          Text(
+                            "Popular",
+                            style: GoogleFonts.openSans(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "List of good popular movies",
+                            style: GoogleFonts.poppins(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w300,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ))
+              ],
+            ));
           }
         }));
   }
