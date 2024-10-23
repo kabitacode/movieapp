8f6ce0929b910eef71fe1d23e6ef51e051caa9a6
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-10-23 09:10:28 +0700

    ✨

--- lib/controller/movie_controller.dart
+++ lib/controller/movie_controller.dart
@@ -2,16 +2,20 @@ import 'dart:convert';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:movieapp/models/genre_model.dart';
 import 'package:movieapp/models/movie_model.dart';
 
 class MovieController extends GetxController {
   var isLoading = true.obs;
+  var loadingGenre = false.obs;
   var list = [].obs;
   var page = 1;
   String? api_key = dotenv.env['API_KEY'];
+  var accessToken = dotenv.env['ACCESS_TOKEN'];
 
+  int? selectedGenre;
   final movieList = <MovieModel>[].obs;
-  final movie = Rxn<MovieModel>();
+  final genreList = <Genres>[].obs;
 
   @override
   void onInit() {
@@ -24,17 +28,21 @@ class MovieController extends GetxController {
     super.dispose();
   }
 
-  void getMovies({int? page}) async {
+  void getMovies({int? page, int? genre}) async {
     isLoading.value = true;
 
-    final String baseUrl =
-        "https://api.themoviedb.org/3/discover/movie?api_key=$api_key&page=${page ?? this.page}";
+    final String baseUrl = genre != null
+        ? "https://api.themoviedb.org/3/discover/movie?api_key=$api_key&page=${page ?? this.page}&with_genres=$genre"
+        : "https://api.themoviedb.org/3/discover/movie?api_key=$api_key&page=${page ?? this.page}";
 
     try {
       final res = await http.get(Uri.parse(baseUrl), headers: {
         'Content-Type': 'application/json',
       });
       var result = json.decode(res.body);
+
+      print(result);
+      print(res.request);
 
       if (res.statusCode == 200) {
         if (page == 1) {
@@ -55,6 +63,33 @@ class MovieController extends GetxController {
       print(e);
     } finally {
       isLoading.value = false;
+    }
+  }
+
+  void getGenre() async {
+    loadingGenre.value = true;
+
+    final String baseUrl =
+        'https://api.themoviedb.org/3/genre/movie/list?language=en';
+
+    try {
+      final res = await http.get(Uri.parse(baseUrl), headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $accessToken'
+      });
+      var result = json.decode(res.body);
+
+      if (res.statusCode == 200) {
+        genreList.value = List<Genres>.from(
+            result['genres'].map((el) => Genres.fromJson(el)));
+      } else {
+        Get.snackbar('error', 'Oops something error!');
+      }
+    } catch (e) {
+      Get.snackbar('error', 'Failed: $e');
+      print(e);
+    } finally {
+      loadingGenre.value = false;
     }
   }
 }
